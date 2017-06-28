9104c7fc30147e689349fcf033716c44708375f2
Gonzalo Ripa <gonza.edelp77@gmail.com>
2017-06-28 17:20:51 -0300

    Ultimo

--- app.js
+++ app.js
@@ -8,29 +8,26 @@ var express = require('express'),
 app = express(); // crea instancia del framework express
 app.use(bodyParser.json()); //se procesan tramas json
 
-const phantom = require('phantom');
+var phantom = require("phantom");
+var _ph, _page, _outObj;
 
-(async function() {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    await page.on("onResourceRequested", function(requestData) {
-        console.info('Requesting', requestData.url)
-    });
-
-    const status = await page.open('http://cielosports.com/nota/73329/no_logra_levantar_cabeza/');
+phantom.create().then(ph => {
+    _ph = ph;
+    return _ph.createPage();
+}).then(page => {
+    _page = page;
+    return _page.open('http://cielosports.com/nota/73329/no_logra_levantar_cabeza/');
+}).then(status => {
     console.log(status);
-
-    page.evaluate(function() {
+    return _page.evaluate(function() {
         return document.getElementsByClassName("cuerpo-nota")[0].getElementsByTagName("p")[0].innerHTML;
     }).then(function(html){
         console.log(html);
         global.content = html;
-    });
-//    const content = await page.property('content');
-//    console.log(content);
-
-    await instance.exit();
-}());
+    }); 
+    _page.close();
+    _ph.exit()
+}).catch(e => console.log(e));
 
 
 router = express.Router(); //crea objeto tipo express.Router, que permite procesar peticiones http
