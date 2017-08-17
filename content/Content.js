a789924640257cf2531e1e53814a9f32fea6e3ee
dssd <gonza.edelp77@gmail.com>
2017-08-17 19:18:37 -0300

    make it better

--- content/Content.js
+++ content/Content.js
@@ -1,5 +1,5 @@
 //Modelo de usuarios
-var mongoose = require('mongoose');  
+/*var mongoose = require('mongoose');  
 var UserSchema = new mongoose.Schema({  
   name: String,
   email: String,
@@ -8,3 +8,4 @@ var UserSchema = new mongoose.Schema({
 mongoose.model('User', UserSchema); //Se vincula el esquema (todos los usuarios con nombre, email y pass) con el modelo llamado User
 //Se utiliza el modelo para acceder a los datos de la base
 module.exports = mongoose.model('User'); //Se exporta para usarlo en otros archivos
+*/
