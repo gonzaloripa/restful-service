16d4f2600f8f0f4290fc7fef7c8342935b207e00
dssd <gonza.edelp77@gmail.com>
2017-08-17 20:01:52 -0300

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Indica en que puerto debe escuchar la app
 var app = require('./app');
 var port = process.env.PORT || 3000;
-//var ip = process.env.IP || 'localhost' ;
+var ip = process.env.IP || 'localhost' ;
 var https= require('https'); 
 
 server = https.createServer(app); //crea servidor http, usa instancia de express
-var server = app.listen(port, function() {  //Arranca el server
+var server = app.listen(port,ip, function() {  //Arranca el server
   console.log('Express server listening on port ' + port);
 });
 
