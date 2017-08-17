d95cde0f9592eb23d8909af3b901ddeb887677cb
dssd <gonza.edelp77@gmail.com>
2017-08-17 19:15:34 -0300

    make it better

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 //Establece la conexion a la base de datos
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://yourMongoDBURIGoesHere');
