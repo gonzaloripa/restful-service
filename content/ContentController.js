8ff61e1d22900f7ab4ea1acb3d8d9631ae439b55
gonzaloripa <gonza.edelp77@gmail.com>
2017-09-11 01:55:10 -0300

    Controlar error de estado del open

--- content/ContentController.js
+++ content/ContentController.js
@@ -25,6 +25,9 @@ body.on('update', function (url,className,res) {
         _page = page;
         return _page.open(url); //'http://cielosports.com/nota/75036/tuvimos_ese_espiritu_ganador_remarco_fernando_zuqui/'
     }).then(status => {
+        if (status == fail){
+           res.redirect('/noticia');
+        }
         console.log(status);
         return _page.evaluate(function(c) {
             var parrafos = document.getElementsByClassName(c)[0].getElementsByTagName("p");//"cuerpo-nota"
